8f4fe151af558cbd1f784ce7ba2fd6116c4984e8
Pau Tomás <tomas10pau@gmail.com>
2023-01-24 13:29:41 +0100

    Middleware validation: login & register

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
 import { body } from "express-validator";
+import { validationResultExpress } from "../middlewares/validationResultExpress.js";
 
 const router = express.Router(); //es un middleware para poder gestionar mejor nuestras rutas en nuestro sitio web.
 //todo lo que es autenticación estará en este router.
@@ -9,19 +10,32 @@ router.post(
   "/register",
   [
     body("email", "Formato email incorrecto").trim().isEmail().normalizeEmail(),
-    body("password", "Formato de pasword incorrecto")
+    body("password", "Mínimo 6 caracteres")
       .trim() //siempre hacer la limpieza primero.
-      .isLength({ min: 6 })
-      .custom((value, { req }) => {
+      .isLength({ min: 6 }),
+    body("password", "Formato de pasword incorrecto").custom(
+      (value, { req }) => {
         if (value !== req.body.repassword) {
           throw new Error("No coinciden las contraseñas");
         }
         return value;
-      }),
+      }
+    ),
   ],
+  validationResultExpress,
   register
 );
 
-router.post("/login", login);
+router.post(
+  "/login",
+  [
+    body("email", "Formato email incorrecto").trim().isEmail().normalizeEmail(),
+    body("password", "Mínimo 6 caracteres")
+      .trim() //siempre hacer la limpieza primero.
+      .isLength({ min: 6 }),
+  ],
+  validationResultExpress,
+  login
+);
 
 export default router; //con el default podemos nombrar el archivo como queramos.
